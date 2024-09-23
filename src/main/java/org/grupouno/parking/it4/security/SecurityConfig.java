ea68919e914fb317b359378ef2b12af86d816ffc
carlosaltan18 <carlosaltan850@gmail.com>
2024-09-23 00:11:49 -0600

    cors actualizado

--- src/main/java/org/grupouno/parking/it4/security/SecurityConfig.java
+++ src/main/java/org/grupouno/parking/it4/security/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
 
